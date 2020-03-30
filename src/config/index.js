2f60cfabda8fd9edb9cdce4c082fc9920784904c
Shakhboz Farkhodov <s.farkhodov@student.inha.uz>
2020-03-30 17:17:19 +0500

    bcrypt deleted from package.json

--- src/config/index.js
+++ src/config/index.js
@@ -5,11 +5,7 @@ const baseConfig = {
   env,
   isDev: env === "development",
   isTest: env === "testing",
-  port: 4000,
-  secrets: {
-    jwt: process.env.JWT_SECRET,
-    jwtExp: "100d"
-  }
+  port: 4000
 };
 
 let envConfig = {};
