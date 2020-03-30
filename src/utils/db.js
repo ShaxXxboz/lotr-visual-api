a894ff4ed3989b0cc671a1f413e7adfe53e5144e
Shakhboz Farkhodov <s.farkhodov@student.inha.uz>
2020-03-30 21:56:26 +0500

    setting up db

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,7 +3,7 @@ import options from "../config";
 
 export const connect = (url = options.dbUrl, opts = {}) => {
   return mongoose.connect(
-    process.env.MONGODB_URI,
+    url,
     { ...opts, useNewUrlParser: true }
   );
 };
