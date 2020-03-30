76ab9efadd87b85d6665c571b3891fefbefde0bc
Shakhboz Farkhodov <s.farkhodov@student.inha.uz>
2020-03-30 20:33:23 +0500

    starting up db

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,7 +3,7 @@ import options from "../config";
 
 export const connect = (url = options.dbUrl, opts = {}) => {
   return mongoose.connect(
-    url,
+    "mongodb+srv://booradley:VWNGCEzf86HBESOa@lotr-db-xbbx7.mongodb.net/test?retryWrites=true&w=majority",
     { ...opts, useNewUrlParser: true }
   );
 };
