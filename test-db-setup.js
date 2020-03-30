f48799ddbf4da44f5c80f19ebcbac68191b9161b
Shakhboz Farkhodov <s.farkhodov@student.inha.uz>
2020-03-30 18:18:42 +0500

    Procfile added

--- test-db-setup.js
+++ test-db-setup.js
@@ -1,33 +1,34 @@
-import mongoose from 'mongoose'
-import cuid from 'cuid'
-import _ from 'lodash'
-import { Item } from './src/resources/item/item.model'
-import { List } from './src/resources/list/list.model'
-import { User } from './src/resources/user/user.model'
+import mongoose from "mongoose";
+import cuid from "cuid";
+import _ from "lodash";
+import { Book } from "./src/resources/book/book.model";
+import { Character } from "./src/resources/character/character.model";
+import { Race } from "./src/resources/race/race.model";
+import { Movie } from "./src/resources/movie/movie.model";
 
-const models = { User, List, Item }
+const models = { Character, Book, Movie, Race };
 
 const url =
   process.env.MONGODB_URI ||
   process.env.DB_URL ||
-  'mongodb://localhost:27017/tipe-devapi-testing'
+  "mongodb://localhost:27017/tipe-devapi-testing";
 
 global.newId = () => {
-  return mongoose.Types.ObjectId()
-}
+  return mongoose.Types.ObjectId();
+};
 
 const remove = collection =>
   new Promise((resolve, reject) => {
     collection.remove(err => {
-      if (err) return reject(err)
-      resolve()
-    })
-  })
+      if (err) return reject(err);
+      resolve();
+    });
+  });
 
 beforeEach(async done => {
-  const db = cuid()
+  const db = cuid();
   function clearDB() {
-    return Promise.all(_.map(mongoose.connection.collections, c => remove(c)))
+    return Promise.all(_.map(mongoose.connection.collections, c => remove(c)));
   }
 
   if (mongoose.connection.readyState === 0) {
@@ -38,24 +39,24 @@ beforeEach(async done => {
           useNewUrlParser: true,
           autoIndex: true
         }
-      )
-      await clearDB()
-      await Promise.all(Object.keys(models).map(name => models[name].init()))
+      );
+      await clearDB();
+      await Promise.all(Object.keys(models).map(name => models[name].init()));
     } catch (e) {
-      console.log('connection error')
-      console.error(e)
-      throw e
+      console.log("connection error");
+      console.error(e);
+      throw e;
     }
   } else {
-    await clearDB()
+    await clearDB();
   }
-  done()
-})
+  done();
+});
 afterEach(async done => {
-  await mongoose.connection.db.dropDatabase()
-  await mongoose.disconnect()
-  return done()
-})
+  await mongoose.connection.db.dropDatabase();
+  await mongoose.disconnect();
+  return done();
+});
 afterAll(done => {
-  return done()
-})
+  return done();
+});
