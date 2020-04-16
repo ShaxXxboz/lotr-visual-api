dac2f0b97f637ac19fbaad4ccd065b20523ea2d8
Shakhboz Farkhodov <s.farkhodov@student.inha.uz>
2020-04-16 20:45:58 +0500

    testing populate

--- src/resources/character/character.router.js
+++ src/resources/character/character.router.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import controllers from "./character.controllers";
-/* import { Character } from './character.model'
+import { Character } from "./character.model";
 
-ArticleSchema.pre('findOne', function () {
-  this.populate('author');
-}); */
+Character.pre("findOne", function() {
+  this.populate("race");
+});
 
 const router = new Router();
 
@@ -13,6 +13,6 @@ router
   .get(controllers.getMany)
   .post(controllers.createOne);
 
-router.route("/:slug").get(controllers.getOne.populate("race").execPopulate());
+router.route("/:slug").get(controllers.getOne);
 
 export default router;
